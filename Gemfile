b199b549d4b9c427c1a4d4431e539654286d8fa1
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-04-17 16:12:13 -0400

    Initial add

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'mongoid', '~> 3.1.3'
+gem 'angular-rails', '~> 0.0.12'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularRails::Application.config.secret_token = '2fdfd7cc0d9bffbd59d596d3dd42101b15730cf9d839dc9f66d7fca2cf2cf0113cac3f207343b12a8d9197644ac2230b2dc514af29c1a7b90bbc0e80292613f4'
