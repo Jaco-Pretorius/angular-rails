cfd8ffe84d58407c4cf120bf8202e2dba0dd61b8
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-04-18 13:33:32 -0400

    Adding basics for creating a new todo item

--- app/assets/javascripts/controllers/todo_controller.js
+++ app/assets/javascripts/controllers/todo_controller.js
@@ -1,8 +1,20 @@
-function TodoController($scope, $http) {
+function TodoController($scope, $http, $location) {
  
   $http.get('/todo/index.json').success(function(data) {
     $scope.items = data;
   });
+
+  $scope.newTodo = function() {
+    $location.path("new");
+  };
+
+  $scope.addTodo = function() {
+    alert('add todo');
+  }
+
+  $scope.backToList = function() {
+    $location.path("list");
+  }
  
 }
 
--- app/assets/javascripts/modules/todo_app.js
+++ app/assets/javascripts/modules/todo_app.js
@@ -3,14 +3,19 @@ var todoApp = angular.module('todoApp', [])
 todoApp.config(function ($routeProvider) {
   $routeProvider
     .when('/list',
-        {
-            controller: 'TodoController',
-            template: JST['views/list']()
-        })
+      {
+        controller: 'TodoController',
+        template: JST['views/list']()
+      })
+    .when('/new',
+      {
+        controller: 'TodoController',
+        template: JST['views/new']()
+      })
     .when('/item/:itemID',
-        {
-            controller: 'TodoController',
-            templateUrl: '/app/views/item.html'
-        })
+      {
+        controller: 'TodoController',
+        templateUrl: '/app/views/item.html'
+      })
     .otherwise({ redirectTo: '/list' });
 });
